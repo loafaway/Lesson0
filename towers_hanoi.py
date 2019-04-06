8c0e65fc7db9dc6b0311cad5543b0fa2080014b1
loafaway <48787212+loafaway@users.noreply.github.com>
2019-04-06 10:23:04 +0800

    Update towers_hanoi.py

--- towers_hanoi.py
+++ towers_hanoi.py
@@ -90,7 +90,7 @@ game_over = False
 
 WHITE = (255, 255, 255)
 ORANGE = (200, 200, 0)
-GREEN = (0, 255, 0)
+PURPLE = (0, 255, 255)
 BLUE = (0, 0, 190)
 BLACK = (50, 50, 50)
 GREY = (180, 180, 180) 
@@ -110,11 +110,11 @@ fontObj3.set_underline(True)
 textSurfObj = fontObj2.render('遞迴示例：六層河內塔', True, WHITE, BLUE)
 textRectObj = textSurfObj.get_rect()
 
-pegSurfObj1 = fontObj1.render('A', True, ORANGE, WHITE)
+pegSurfObj1 = fontObj1.render('A', True, ORANGE, PURPLE)
 pegRectObj1 = pegSurfObj1.get_rect()
-pegSurfObj2 = fontObj1.render('B', True, ORANGE, WHITE)
+pegSurfObj2 = fontObj1.render('B', True, ORANGE, PURPLE)
 pegRectObj2 = pegSurfObj2.get_rect()
-pegSurfObj3 = fontObj1.render('C', True, ORANGE, WHITE)
+pegSurfObj3 = fontObj1.render('C', True, ORANGE, PURPLE)
 pegRectObj3 = pegSurfObj3.get_rect()
 
 textSurfObj0 = fontObj3.render('', True, BLACK, WHITE)
