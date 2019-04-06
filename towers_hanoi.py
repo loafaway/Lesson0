469777dbb0dce0bf01d6f8bc20b1518debe7996f
loafaway <48787212+loafaway@users.noreply.github.com>
2019-04-07 07:56:11 +0800

    Update towers_hanoi.py

--- towers_hanoi.py
+++ towers_hanoi.py
@@ -4,7 +4,7 @@ import pygame
 
 def towers(n):
     
-    #global TOH
+    global TOH
     TOH = []
     toh(n)
     
@@ -40,7 +40,7 @@ def drawAll(peg):
 
 def setRoute(n, fro, to):
     
-    global XY
+    #global XY
     XY = []
 
     # vertical movement on the source position
