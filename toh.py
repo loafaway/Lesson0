819ccff6de05aee890c1f914102afb47e511cdd5
loafaway <48787212+loafaway@users.noreply.github.com>
2019-04-07 09:18:03 +0800

    Update toh.py

--- toh.py
+++ toh.py
@@ -96,7 +96,7 @@ def setRoute(n, fro, to):
 
 pygame.init()
 surface = pygame.display.set_mode((600, 400))
-pygame.display.set_caption('The Tower of Hanoi Demo!')
+pygame.display.set_caption('"The Tower of Hanoi of 6 Levels" Demo!')
 
 FPS = 60
 ADJ = -50
